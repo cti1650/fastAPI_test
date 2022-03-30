00b7951845d1e175081fbb18715268e967a43828
cti1650 <cti1650@gmail.com>
2022-03-30 15:07:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-
-
-if __name__ == "__main__":
-    debug: bool = True if os.getenv("ENV", "dev") == "dev" else False
-    port: int = int(os.getenv("PORT", 5000))
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=debug, debug=debug)
