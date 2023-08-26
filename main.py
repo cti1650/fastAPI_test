d9e3c0886ad5f0de994b6393087d0b9388437dd5
cti1650 <cti1650@gmail.com>
2023-08-26 18:34:19 +0000

    feat: add cloudfunction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from typing import Optional
 
+from agraffe import Agraffe, Service
+
 from fastapi import FastAPI
 from routers.health import api_health
 
@@ -15,3 +17,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+platform = os.environ.get("PLATFORM", "")
+
+if platform == "GCP":
+    entry_point = Agraffe.entry_point(app, Service.google_cloud_functions)
+elif platform == "AWS":
+    entry_point = Agraffe.entry_point(app, Service.aws_lambda)
+else:
+    Exception(f"Unsupported platform of {platform}")
